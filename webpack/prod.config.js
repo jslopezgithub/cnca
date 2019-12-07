da9366ecf17aa5122f3935668373e677c8bccf68
million <48320653+milleymilli@users.noreply.github.com>
2019-12-07 16:18:58 +0100

    Added client folder

---
+++ webpack/prod.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackExternalsPlugin = require("html-webpack-externals-plugin");
+const path = require("path");
+const merge = require("webpack-merge");
+
+const common = require("./common.config");
+
+module.exports = merge(common, {
+	devtool: "source-map",
+	mode: "production",
+	optimization: {
+		runtimeChunk: "single",
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: "vendor",
+					chunks: "all",
+				},
+			},
+		},
+	},
+	output: {
+		filename: "[name].[contenthash].js",
+		path: path.resolve(__dirname, "../../dist/static"),
+	},
+	plugins: [
+		new HtmlWebpackExternalsPlugin({
+			externals: [
+				{
+					global: "React",
+					entry: {
+						path: "https://unpkg.com/react@16.8/umd/react.production.min.js",
+						attributes: {
+							crossorigin: "",
+						},
+					},
+					module: "react",
+				},
+				{
+					global: "ReactDOM",
+					entry: {
+						path: "https://unpkg.com/react-dom@16.8/umd/react-dom.production.min.js",
+						attributes: {
+							crossorigin: "",
+						},
+					},
+					module: "react-dom",
+				},
+			],
+		}),
+	],
+});
