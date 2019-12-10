90f9e2ede73b20d6a79f5310036af71b2659eb45
milleymilli <milleylove143@gmail.com>
2019-12-10 15:18:39 +0100

    deploying to heroku

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
 const http = require('http');
 const chalk = require('chalk');
 const app = require('./app');
@@ -6,6 +8,7 @@ const app = require('./app');
 // eslint-disable-next-line radix
 const port = parseInt(process.env.PORT || '5000');
 
+// eslint-disable-next-line consistent-return
 app.use((err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
@@ -15,6 +18,13 @@ app.use((err, req, res, next) => {
 });
 
 const server = http.createServer(app);
+if (process.env.NODE.ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 server.listen(port);
 
