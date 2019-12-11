3a9ed5845a8dd1b99e487a49864bbf9263a8f405
milleymilli <milleylove143@gmail.com>
2019-12-11 16:20:40 +0100

    removing static folder

---
+++ client/build/precache-manifest.905d530cb1be6c07cafff1aab85ea4fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96944a50cc41d7e0ee29e0b45c40f428",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fbb7ecf2467ab804ce3",
+    "url": "/static/css/main.fa70d9ed.chunk.css"
+  },
+  {
+    "revision": "57f4d568b46a0236f4a6",
+    "url": "/static/js/2.4dd90c32.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.4dd90c32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fbb7ecf2467ab804ce3",
+    "url": "/static/js/main.bb13a7af.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
