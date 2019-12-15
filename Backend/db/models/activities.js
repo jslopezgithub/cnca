2187ad33158f933c35706efd86dec53cceb9249e
= <=>
2019-12-15 23:25:52 +0100

    backend commit:
    1. created db model file: users.js
    2. created db model file: roles.js
    3. created db model file: activities.js
    4. created db model file: activity-types.js
    5. created file: dbconfig.js with connection config
    
    Created tables: users_tokens to store auth tokens
    Created tables: holidays to store holidays
    
    updated dbdiagram.JPG
    updated install.sql / added tables

---
+++ Backend/db/models/activities.js
@@ -0,0 +1,32 @@
+const Sequelize   = require('sequelize');
+const DataType    = require('sequelize').DataTypes;
+
+const config = require('../configs/sqldbconfig');
+
+const sequelize = new Sequelize(config.database, config.user, config.password, {
+    host: config.host,
+    dialect: config.type
+});
+
+
+const Activities = sequelize.define('activities', {
+    
+    volonteer_id: {
+        type: DataType.INTEGER,
+
+        allowNull: false,
+        references: {
+            model: users,
+            key: 'id'
+        }
+    },
+
+    id: {
+        type:       DataType.INTEGER, 
+        primaryKey: true 
+    },
+
+    name: DataType.STRING(64) 
+});
+
+module.exports = Activities;
--- docs/database/install.sql
+++ docs/database/install.sql
@@ -5,99 +5,69 @@ create database cnca;
 
 USE cnca;
 
-CREATE TABLE roles (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(64) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-AVG_ROW_LENGTH = 8192
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE `holidays` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=8192;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `codice_fisicale` varchar(32) DEFAULT NULL,
+  `nome` varchar(128) NOT NULL,
+  `cognome` varchar(128) DEFAULT NULL,
+  `data_di_nascita` date DEFAULT NULL,
+  `sesso` bit(1) DEFAULT b'1',
+  `organizzazione` varchar(255) DEFAULT NULL,
+  `commune_di_nascita` varchar(255) DEFAULT NULL,
+  `password` varchar(128) DEFAULT NULL,
+  `role_id` int(11) DEFAULT 3,
+  `email` varchar(64) NOT NULL,
+  `image` mediumblob DEFAULT NULL,
+  `createdAt` datetime DEFAULT current_timestamp(),
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`,`email`),
+  UNIQUE KEY `UK_users_id` (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `FK_users_roles_id` (`role_id`),
+  CONSTRAINT `FK_users_roles_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+  CONSTRAINT `user_role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE users (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `codice_fisicale` VARCHAR(32) NULL DEFAULT NULL,
-  `nome` VARCHAR(128) NOT NULL,
-  `cognome` VARCHAR(128) NULL DEFAULT NULL,
-  `data_di_nascita` DATE NULL DEFAULT NULL,
-  `sesso` BIT(1) NULL DEFAULT b'1',
-  `organizzazione` VARCHAR(255) NULL DEFAULT NULL,
-  `commune_di_nascita` VARCHAR(255) NULL DEFAULT NULL,
-  `password` VARCHAR(128) NULL DEFAULT NULL,
-  `role_id` INT(11) NULL DEFAULT 3,
-  `email` VARCHAR(50) NOT NULL,
-  `image` MEDIUMBLOB NULL DEFAULT NULL,
-  `createdAt` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
-  `updatedAt` DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (`id`, `email`),
-  UNIQUE INDEX `UK_users_id` (`id` ASC),
-  INDEX `FK_users_roles_id` (`role_id` ASC),
-  CONSTRAINT `user_role_id`
-    FOREIGN KEY (role_id)
-    REFERENCES roles (id))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE `activity_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE users 
-  ADD UNIQUE INDEX email(email);
-
-ALTER TABLE users 
-  ADD CONSTRAINT FK_users_roles_id FOREIGN KEY (role_id)
-    REFERENCES roles(id);
-
-CREATE TABLE activity_category (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(128) DEFAULT NULL,
-  PRIMARY KEY (id)
-)
-ENGINE = INNODB,
-CHARACTER SET utf8,
-COLLATE utf8_general_ci;
-
-CREATE TABLE IF NOT EXISTS activity (
-  `id` INT(11) NOT NULL,
-  `volonteer_id`  INT(11) NOT NULL,
-  `activity_date` DATE NULL DEFAULT NULL,
-  `start_time`    TIME NULL DEFAULT NULL,
-  `end_time`      TIME NULL DEFAULT NULL,
-  `activity_category_id` INT(11) NOT NULL,
-
+CREATE TABLE `activities` (
+  `id` int(11) NOT NULL,
+  `volunteer_id` int(11) NOT NULL,
+  `activity_date` date DEFAULT NULL,
+  `start_time` time DEFAULT NULL,
+  `end_time` time DEFAULT NULL,
+  `activity_type_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  
-  INDEX `activity_category_type` (`activity_category_id` ASC),
-  INDEX `FK_activity_users_id` (`volonteer_id` ASC),
-  
-  CONSTRAINT `activity_type_id`
-  FOREIGN KEY (`activity_category_id`)
-  REFERENCES activity_category (`id`),
-  
-  CONSTRAINT `activity_owner_id`
-  FOREIGN KEY (`volonteer_id`)
-  REFERENCES users (id))
-  
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  KEY `activity_type_id` (`activity_type_id`),
+  KEY `FK_activity_users_id` (`volunteer_id`),
+  CONSTRAINT `activity_owner_id` FOREIGN KEY (`volunteer_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `activity_type_id` FOREIGN KEY (`activity_type_id`) REFERENCES `activity_types` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE activity 
-  ADD CONSTRAINT FK_activity_activity_category_id FOREIGN KEY (activity_category_id)
-    REFERENCES activity_category(id);
-
-ALTER TABLE activity 
-  ADD CONSTRAINT FK_activity_users_id FOREIGN KEY (volonteer_id)
-    REFERENCES users(id);
-
-
--- DEFAULT ROLES 
-INSERT INTO roles VALUES
-(1, 'owner'),
-(2, 'admin'),
-(3, 'volonteer');
-
-
--- ADDING TEST USERS
--- INSERT INTO users(codice_fisicale,nome,cognome,data_di_nascita,sesso,organizzazione,commune_di_nascita,password,role_id,email) VALUES
--- ('aeh8933j3f939', 'Ramazan', 'Aliskhanov', '1986-06-07', True, 'ETA BETA', 'NETTUNO', '123456', 1, 'ravillion@mail.ru'),
--- ('aeh8933j3f931', 'Abdulla', 'Ahlamonov', '1986-06-07',  True, 'ETA BETA', 'NETTUNO', '123456', 2, 'ravillion1@mail.ru'),
--- ('aeh8933j3f933', 'Rizvan', 'Adilkhanov', '1986-06-07',  True, 'ETA BETA', 'NETTUNO', '123456', 3, 'ravillion2@mail.ru');
+CREATE TABLE `users_tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login_date` timestamp NULL DEFAULT current_timestamp(),
+  `user_id` int(11) DEFAULT NULL,
+  `token_string` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_token_owner` (`user_id`),
+  CONSTRAINT `user_token_owner` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
