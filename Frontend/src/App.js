187c357efe9ae7c7577388543edd012a10d68108
milleymilli <milleylove143@gmail.com>
2019-12-22 23:10:40 +0100

    remain styling and func. for the 2nd sub comp. for firstPage

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
       <div>
         <Navbar />
         {/* <Registration /> */}
-        <SecondCompMain />
+        {/* <SecondCompMain /> */}
         {/* <UserInfo /> */}
         <Footer />
       </div>
