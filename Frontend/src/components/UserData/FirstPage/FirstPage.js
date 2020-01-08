3fd13211b134a8003555fabfd6a501a5f08baa13
Hlal <mzolldic77@gmail.com>
2020-01-08 17:58:09 +0100

    added a date picker to chose date

--- Frontend/src/components/UserData/FirstPage/FirstPage.js
+++ Frontend/src/components/UserData/FirstPage/FirstPage.js
@@ -12,17 +12,17 @@ import Day5 from './Calenderbody/Day5';
 import Day6 from './Calenderbody/Day6';
 import Day7 from './Calenderbody/Day7';
 // import Columns from "./columns";
-// import '../../FirstPage/FirstPage.css';
+import '../../FirstPage/FirstPage.css';
 
 export default function FirstPage() {
   return (
-    <div className="FirstPage">
+    <div className='FirstPage'>
       <TotalHours />
       <UserName />
       <TotalTime />
-      <div className="daystyling"></div>
-      <div className="ingressobox">INGRESSO</div>
-      <div className="uscitabox">USCITA</div>
+      <div className='daystyling'></div>
+      <div className='ingressobox'>INGRESSO</div>
+      <div className='uscitabox'>USCITA</div>
       <Day1 />
       <Day2 />
       <Day3 />
--- Frontend/src/components/UserData/FirstPage/UserName.js
+++ Frontend/src/components/UserData/FirstPage/UserName.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './Username.css';
+import Datepicker from './Datepicker';
 
 export default function UserName() {
   const FirstName = 'First name';
   const SecondName = 'SecondName';
   return (
     <div className='UserName'>
-      <div className='calender'>
-        <i className='fas fa-calendar-alt calenderIcon'></i>
-      </div>
+      <Datepicker />
       <div className='nameInput'>
         <span>{FirstName}</span>
         <span>{SecondName}</span>
